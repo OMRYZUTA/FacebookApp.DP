33d0566dcf5248f0971298c53c65c87ce2db290b
Omri Zuta <omrizu@mta.ac.il>
2020-11-21 19:37:59 +0200

    added methods in album creator

--- FacebookApp/FacebookApp/AlbumCreator.cs
+++ FacebookApp/FacebookApp/AlbumCreator.cs
@@ -1,6 +1,62 @@
-﻿namespace FacebookApp
+﻿using FacebookWrapper.ObjectModel;
+using System;
+using System.Collections.Generic;
+
+namespace FacebookApp
 {
-    internal class AlbumCreator
+    public class AlbumCreator
     {
+        List<Album> createdAlbums;
+        User m_LoggedInUser;
+        public AlbumCreator()
+        {
+
+        }
+
+        public void CreateAlbumWith(string i_FriendName)
+        {
+            bool isFriendInList = isFriendInLoggedInUserList(i_FriendName);
+            if (isFriendInList)
+            {
+                bool isAlbumExist = isAlbumCreated(i_FriendName);
+                if (!isAlbumExist)
+                {
+                    generateNewAlbum(i_FriendName);
+                }
+            }
+        }
+
+        private bool isAlbumCreated(string i_FriendName)
+        {
+            bool result = false;
+            foreach (Album album in createdAlbums)
+            {
+                if (album.Name == i_FriendName)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        private void generateNewAlbum(string i_FriendName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool isFriendInLoggedInUserList(string i_FriendName)
+        {
+           bool result = false;
+            foreach(User friend in m_LoggedInUser.Friends)
+            {
+                if(friend.Name == i_FriendName)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
     }
 }
